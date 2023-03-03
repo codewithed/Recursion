4b91a5e652004b7c0849295375746ed1baf1fc3f
Edmund Debrah <edmundking05@gmail.com>
2023-03-03 13:10:15 +0000

    add fibs.js

---
+++ fibs.js
@@ -0,0 +1,15 @@
+function fibs(num) {
+  const arr = [0, 1];
+
+  if (num === 0) {
+    return [];
+  }
+
+  if (num === 1) { return arr[0]; }
+
+  for (let i = 2; i < num; i += 1) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr;
+}
+console.log(fibs(10));
